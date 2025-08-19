cb35f76cb3a4f93694420a4ec81d41974d0358e5
Robert Cowher <bobcowher@gmail.com>
2025-08-18 22:16:09 -0500

    ROS Install

--- setup.sh
+++ setup.sh
@@ -9,5 +9,20 @@ sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-sudo apt install software-properties-common
-sudo add-apt-repository universe
+sudo apt install software-properties-common -y
+sudo add-apt-repository universe -y
+
+sudo apt update && sudo apt install curl -y
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
+sudo dpkg -i /tmp/ros2-apt-source.deb
+
+sudo apt update -y && sudo apt install ros-dev-tools -y
+
+sudo apt update -y
+
+sudo apt upgrade -y
+
+#sudo apt install ros-jazzy-desktop
+
+sudo apt install ros-jazzy-ros-base
