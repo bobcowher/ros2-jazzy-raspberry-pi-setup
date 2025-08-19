a747b0a648ca674b67254518a72d2f820c3c8c9b
Robert Cowher <bobcowher@gmail.com>
2025-08-18 22:10:42 -0500

    setup

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo usermod -a -G dialout $USER
+sudo apt install bluez -y
+bluetoothctl power on
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
